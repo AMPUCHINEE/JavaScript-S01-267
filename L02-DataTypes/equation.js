cbed65394ed00dc9bba16ff7654f5d746ceebf2e
AMPUCHINEE <s6506021620113@email.kmutnb.ac.th>
2024-12-04 11:17:02 +0700

    4

---
+++ L02-DataTypes/equation.js
@@ -0,0 +1,7 @@
+function equation(eq) {
+    const result = eval(eq);
+    return result;
+}
+console.log(equation("1+1"));
+console.log(equation("7*4-2"));
+console.log(equation("1+1+1+1+1"));
---
+++ L02-DataTypes/findPerimeter.js
@@ -0,0 +1,13 @@
+/*const findPerimeter = (length,width) => {
+    const result = (length+length)+(width+width);
+    return result;
+}
+console.log(findPerimeter(6,7));*/
+
+function findPerimeter(length,width) {
+    const result = (length+length)+(width+width);
+    return result;
+}
+console.log(findPerimeter(6,7));
+console.log(findPerimeter(20,10));
+console.log(findPerimeter(2,9));
