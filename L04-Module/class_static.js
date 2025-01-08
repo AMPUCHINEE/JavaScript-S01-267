1f44b1fa685d5d84fc3f2c5c8bfbf0b4bf05f5e5
AMPUCHINEE <s6506021620113@email.kmutnb.ac.th>
2025-01-08 10:15:15 +0700

    four

---
+++ L04-Module/class_static.js
@@ -0,0 +1,17 @@
+class dog {
+    constructor(name) {
+        this._name = name;
+    }
+    introduce() {
+        console.log('This is '+ this._name + " !");
+    }
+
+    static bark() {
+        console.log("Woof!");
+    }
+}
+const myDog = new Dog("Buster");
+myDog.introduce();
+
+//Dog.bark();
+Dog.introduce();
